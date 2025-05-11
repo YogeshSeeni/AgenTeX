f3b17a8409741b23b475b4974ff15d10b801fae8
Yogesh Seenichamy <yogeshseeni60@gmail.com>
2025-05-11 14:53:30 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Initialize the OpenAI client with API key from environment variables
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    input=[{
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": "what's in this image?"},
+            {
+                "type": "input_image",
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+        ],
+    }],
+)
+
+print(response.output_text)
